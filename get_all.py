e92ff539b422a4182a7c69952ec6c8db7b39f444
grafst <grafst@users.noreply.github.com>
2025-07-07 10:35:46 +0200

    get all newsletters

--- get_all.py
+++ get_all.py
@@ -4,6 +4,7 @@ from icecream import ic
 from csv import DictReader
 from browser import full_page_screenshot
 from tqdm import tqdm
+import os
 
 
 newsletters=DictReader(open("newsletters.csv"))
@@ -12,4 +13,6 @@ for newsletter in tqdm(newsletters):
     if newsletter["url"]!='':
         ic(newsletter)
         path=f"newsletters/{newsletter['id']}.jpg"
-        full_page_screenshot(newsletter["url"],path)
+        # does path exist?
+        if not os.path.isfile(path):
+            full_page_screenshot(newsletter["url"],path)
